2aa40e4a59f2f13ccb6152b131e1c10d0fcde4fd
Linda Zheng <lindazheng1993@gmail.com>
2020-11-30 05:51:58 -0600

    Update scraper_configs.py

--- scraper_configs.py
+++ scraper_configs.py
@@ -1,67 +1,67 @@
 CONFIGS = [
-    # {
-    #     "show": "South Park",
-    #     "imdb_url" : "https://www.imdb.com/title/tt0121955/episodes?season=",
-    #     "num_seasons" : 24,
-    #     "line_search" : '<div class="ipl-rating-star small">',
-    #     "line_start" : '<span class="ipl-rating-star__rating">',
-    #     "line_end" : '</span>',
-    #     "data_file_name" : "southpark"
-    # },
-    # {
-    #     "show": "The Simpsons",
-    #     "imdb_url" : "https://www.imdb.com/title/tt0096697/episodes?season=",
-    #     "num_seasons" : 32,
-    #     "line_search" : '<div class="ipl-rating-star small">',
-    #     "line_start" : '<span class="ipl-rating-star__rating">',
-    #     "line_end" : '</span>',
-    #     "data_file_name" : "simpsons"
-    # },
-    # {
-    #     "show": "Grey's Anatomy",
-    #     "imdb_url" : "https://www.imdb.com/title/tt0413573/episodes?season=",
-    #     "num_seasons" : 17,
-    #     "line_search" : '<div class="ipl-rating-star small">',
-    #     "line_start" : '<span class="ipl-rating-star__rating">',
-    #     "line_end" : '</span>',
-    #     "data_file_name" : "greysanatomy"
-    # },
-    # {
-    #     "show": "Family Guy",
-    #     "imdb_url" : "https://www.imdb.com/title/tt0182576/episodes?season=",
-    #     "num_seasons" : 21,
-    #     "line_search" : '<div class="ipl-rating-star small">',
-    #     "line_start" : '<span class="ipl-rating-star__rating">',
-    #     "line_end" : '</span>',
-    #     "data_file_name" : "familyguy"
-    # },
-    # {
-    #     "show": "Pokemon",
-    #     "imdb_url" : "https://www.imdb.com/title/tt0168366/episodes?season=",
-    #     "num_seasons" : 23,
-    #     "line_search" : '<div class="ipl-rating-star small">',
-    #     "line_start" : '<span class="ipl-rating-star__rating">',
-    #     "line_end" : '</span>',
-    #     "data_file_name" : "pokemon"
-    # },
-    # {
-    #     "show": "Friends",
-    #     "imdb_url" : "https://www.imdb.com/title/tt0108778/episodes?season=",
-    #     "num_seasons" : 10,
-    #     "line_search" : '<div class="ipl-rating-star small">',
-    #     "line_start" : '<span class="ipl-rating-star__rating">',
-    #     "line_end" : '</span>',
-    #     "data_file_name" : "friends"
-    # },
-    # {
-    #     "show": "Bob's Burgers",
-    #     "imdb_url" : "https://www.imdb.com/title/tt1561755/episodes?season=",
-    #     "num_seasons" : 11,
-    #     "line_search" : '<div class="ipl-rating-star small">',
-    #     "line_start" : '<span class="ipl-rating-star__rating">',
-    #     "line_end" : '</span>',
-    #     "data_file_name" : "bobsburgers"
-    # },
+    {
+        "show": "South Park",
+        "imdb_url" : "https://www.imdb.com/title/tt0121955/episodes?season=",
+        "num_seasons" : 24,
+        "line_search" : '<div class="ipl-rating-star small">',
+        "line_start" : '<span class="ipl-rating-star__rating">',
+        "line_end" : '</span>',
+        "data_file_name" : "southpark"
+    },
+    {
+        "show": "The Simpsons",
+        "imdb_url" : "https://www.imdb.com/title/tt0096697/episodes?season=",
+        "num_seasons" : 32,
+        "line_search" : '<div class="ipl-rating-star small">',
+        "line_start" : '<span class="ipl-rating-star__rating">',
+        "line_end" : '</span>',
+        "data_file_name" : "simpsons"
+    },
+    {
+        "show": "Grey's Anatomy",
+        "imdb_url" : "https://www.imdb.com/title/tt0413573/episodes?season=",
+        "num_seasons" : 17,
+        "line_search" : '<div class="ipl-rating-star small">',
+        "line_start" : '<span class="ipl-rating-star__rating">',
+        "line_end" : '</span>',
+        "data_file_name" : "greysanatomy"
+    },
+    {
+        "show": "Family Guy",
+        "imdb_url" : "https://www.imdb.com/title/tt0182576/episodes?season=",
+        "num_seasons" : 21,
+        "line_search" : '<div class="ipl-rating-star small">',
+        "line_start" : '<span class="ipl-rating-star__rating">',
+        "line_end" : '</span>',
+        "data_file_name" : "familyguy"
+    },
+    {
+        "show": "Pokemon",
+        "imdb_url" : "https://www.imdb.com/title/tt0168366/episodes?season=",
+        "num_seasons" : 23,
+        "line_search" : '<div class="ipl-rating-star small">',
+        "line_start" : '<span class="ipl-rating-star__rating">',
+        "line_end" : '</span>',
+        "data_file_name" : "pokemon"
+    },
+    {
+        "show": "Friends",
+        "imdb_url" : "https://www.imdb.com/title/tt0108778/episodes?season=",
+        "num_seasons" : 10,
+        "line_search" : '<div class="ipl-rating-star small">',
+        "line_start" : '<span class="ipl-rating-star__rating">',
+        "line_end" : '</span>',
+        "data_file_name" : "friends"
+    },
+    {
+        "show": "Bob's Burgers",
+        "imdb_url" : "https://www.imdb.com/title/tt1561755/episodes?season=",
+        "num_seasons" : 11,
+        "line_search" : '<div class="ipl-rating-star small">',
+        "line_start" : '<span class="ipl-rating-star__rating">',
+        "line_end" : '</span>',
+        "data_file_name" : "bobsburgers"
+    },
     {
         "show": "It's Always Sunny in Philadelphia",
         "imdb_url" : "https://www.imdb.com/title/tt0472954/episodes?season=",
