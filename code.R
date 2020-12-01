a2ccf1c76a177fc64fab04592157e18e1e128a2c
Linda Zheng <lindazheng@Lindas-Macbook-Pro.local>
2020-11-30 18:53:53 -0600

    recolored

--- code.R
+++ code.R
@@ -9,6 +9,112 @@ library(scales)
 PLOT_SAVE_PATH = "~/Developer/episode-ratings/plots"
 
 #==============================================================================
+# HELPER FUNCTIONS
+#==============================================================================
+
+#--------------------------------------
+# Plot and save combined data with facet_wrap
+#--------------------------------------
+do_facet_plot = function(data, title, subtitle) {
+  ggplot(
+    data, 
+    aes(
+      x = as.factor(season), 
+      y = factor(episode, levels = rev(sort(unique(episode)))), 
+      fill = rating
+    )
+  ) + 
+    facet_wrap(
+      vars(name),
+      scales = "free",
+      ncol = 3,
+    ) +
+    geom_tile(color = "white") + 
+    scale_fill_gradient2(
+      low = "red", 
+      mid = "white", 
+      high = "green", 
+      midpoint = median(data$rating)
+    ) + 
+    labs(
+      x = "Season",
+      y = "Episode",
+      title = title,
+      subtitle = subtitle,
+      caption = "Visualization by @yaylinda",
+      fill = "Rating"
+    ) + 
+    theme(
+      panel.grid.major = element_blank(), 
+      panel.grid.minor = element_blank(), 
+      axis.ticks = element_blank(),
+      strip.text.y = element_text(size = 12, angle = 180),
+      strip.text.x = element_text(size = 12),
+      plot.title = element_text(size = 24),
+      aspect.ratio = 1,
+      plot.margin = margin(0.5, 0, 0.5, 0, "in"),
+    )
+  
+  ggsave(
+    "combined.png",
+    path = PLOT_SAVE_PATH,
+    dpi = 320,
+    width = 17,
+    height = 17,
+    device = "png",
+    units = "in"
+  )
+}
+
+#--------------------------------------
+# Plot and save one dataset (one TV show)
+#--------------------------------------
+do_one_plot = function(data, title, height = 10, width = 10) {
+  ggplot(
+    data, 
+    aes(
+      x = as.factor(season), 
+      y = factor(episode, levels = rev(sort(unique(episode)))), 
+      fill = rating
+    )
+  ) + 
+    geom_tile(color = "white") + 
+    scale_fill_gradient2(
+      low = "red", 
+      mid = "white", 
+      high = "green", 
+      midpoint = median(data$rating)
+    ) + 
+    coord_equal(ratio = 1) + 
+    labs(
+      x = "Season",
+      y = "Episode",
+      title = title,
+      subtitle = "From IMDb User Ratings",
+      caption = "Visualization by @yaylinda",
+      fill = "Rating"
+    ) + 
+    theme(
+      panel.grid.major = element_blank(), 
+      panel.grid.minor = element_blank(), 
+      axis.ticks = element_blank(),
+      strip.text.y = element_text(size = 12, angle = 180),
+      strip.text.x = element_text(size = 12),
+      plot.title = element_text(size = 20)
+    )
+  
+  ggsave(
+    paste(gsub("'", "", gsub(" ", "", title, fixed = TRUE)), ".png", sep = ""), 
+    path = PLOT_SAVE_PATH,
+    dpi = 320,
+    width = width,
+    height = height,
+    device = "png",
+    units = "in"
+  )
+}
+
+#==============================================================================
 # READ DATA
 #==============================================================================
 
@@ -81,108 +187,4 @@ for (show_name in unique(combined$name)) {
 
 do_one_plot(pokemon, "Pokemon", height = 12, width = 12)
 
-#==============================================================================
-# HELPER FUNCTIONS
-#==============================================================================
 
-#--------------------------------------
-# Plot and save combined data with facet_wrap
-#--------------------------------------
-do_facet_plot = function(data, title, subtitle) {
-  ggplot(
-    data, 
-    aes(
-      x = as.factor(season), 
-      y = factor(episode, levels = rev(sort(unique(episode)))), 
-      fill = rating
-    )
-  ) + 
-  facet_wrap(
-    vars(name),
-    scales = "free",
-    ncol = 3,
-  ) +
-  geom_tile(color = "white") + 
-  scale_fill_gradient2(
-    low = muted("red"), 
-    mid = "white", 
-    high = muted("green"), 
-    midpoint = median(data$rating)
-  ) + 
-  labs(
-    x = "Season",
-    y = "Episode",
-    title = title,
-    subtitle = subtitle,
-    caption = "Visualization by @yaylinda",
-    fill = "Rating"
-  ) + 
-  theme(
-    panel.grid.major = element_blank(), 
-    panel.grid.minor = element_blank(), 
-    axis.ticks = element_blank(),
-    strip.text.y = element_text(size = 12, angle = 180),
-    strip.text.x = element_text(size = 12),
-    plot.title = element_text(size = 24),
-    aspect.ratio = 1,
-    plot.margin = margin(0.5, 0, 0.5, 0, "in"),
-  )
-  
-  ggsave(
-    "combined.png",
-    path = PLOT_SAVE_PATH,
-    dpi = 320,
-    width = 17,
-    height = 17,
-    device = "png",
-    units = "in"
-  )
-}
-
-#--------------------------------------
-# Plot and save one dataset (one TV show)
-#--------------------------------------
-do_one_plot = function(data, title, height = 10, width = 10) {
-  ggplot(
-    data, 
-    aes(
-      x = as.factor(season), 
-      y = factor(episode, levels = rev(sort(unique(episode)))), 
-      fill = rating
-    )
-  ) + 
-  geom_tile(color = "white") + 
-  scale_fill_gradient2(
-    low = muted("red"), 
-    mid = "white", 
-    high = muted("green"), 
-    midpoint = median(data$rating)
-  ) + 
-  coord_equal(ratio = 1) + 
-  labs(
-    x = "Season",
-    y = "Episode",
-    title = title,
-    subtitle = "From IMDb User Ratings",
-    caption = "Visualization by @yaylinda",
-    fill = "Rating"
-  ) + 
-  theme(
-    panel.grid.major = element_blank(), 
-    panel.grid.minor = element_blank(), 
-    axis.ticks = element_blank(),
-    strip.text.y = element_text(size = 12, angle = 180),
-    strip.text.x = element_text(size = 12),
-    plot.title = element_text(size = 20)
-  )
-  
-  ggsave(
-    paste(gsub("'", "", gsub(" ", "", title, fixed = TRUE)), ".png", sep = ""), 
-    path = PLOT_SAVE_PATH,
-    dpi = 320,
-    width = width,
-    height = height,
-    device = "png",
-    units = "in"
-  )
-}
